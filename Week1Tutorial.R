37097f27420baf629cb0ea652b790f485560cd24
Temuera Cavanagh <cavanaghtd@gmail.com>
2020-08-13 19:54:07 +1000

    wk 1 tutorial minor changes

--- Week1Tutorial.R
+++ Week1Tutorial.R
@@ -1,4 +1,4 @@
 plot(rnorm(10), rnorm(10))
 
 library(ggplot2)
-ggplot() + aes(x=rnorm(10), y=rnorm(10)) + geom_point()
+ggplot() + aes(x=rnorm(10), y=rnorm(10)) + geom_point() + geom_smooth(method = 'lm', se = TRUE) + ggtitle("Randomised Chart")
